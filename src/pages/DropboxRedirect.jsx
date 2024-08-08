aba7cbee7403cd08da8da8e00b4722633a7d2829
Thinh Nguyen <thinh.nguyen5@sotatek.com>
2024-08-08 10:54:44 +0700

    update: get authorize code

--- src/pages/DropboxRedirect.jsx
+++ src/pages/DropboxRedirect.jsx
@@ -1,48 +1,48 @@
-import axios from 'axios';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useSearchParams } from 'react-router-dom';
-import { config } from '../config';
 import { Heading, VStack } from '@chakra-ui/react';
 
 function DropboxRedirect() {
   const [querys] = useSearchParams();
   const authorizeCode = querys.get('code');
 
-  useEffect(() => {
-    if (!authorizeCode) return;
+  console.log(authorizeCode);
 
-    axios
-      .post(
-        `https://www.dropbox.com/oauth2/token`,
-        {},
-        {
-          params: {
-            code: authorizeCode,
-            redirect_uri: config.DROPBOX_REDIRECT_URI,
-            client_id: config.DROPBOX_CLIENT_ID,
-            client_secret: config.DROPBOX_CLIENT_SECRET,
-            grant_type: 'authorization_code',
-          },
-        }
-      )
-      .then(response => console.log(response.data))
-      // .then(data => {
-      //   return axios.post(
-      //     'http://localhost:8000/api/storage-provider',
-      //     {
-      //       ...data,
-      //       type: 'Dropbox',
-      //       connection_label: 'Dropbox-1',
-      //     },
-      //     {
-      //       headers: {
-      //         Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0aGluaHRiQGdtYWlsLmNvbSIsImV4cCI6MTcyMDU4MzE2OSwidHlwZSI6ImFjY2VzcyJ9.r6kbdaUm-m9baDToDHZWRCAzsIr2njS1NQJX1B0zHpw`,
-      //       },
-      //     }
-      //   );
-      // })
-      .catch(error => console.log(error));
-  }, [authorizeCode]);
+  // useEffect(() => {
+  //   if (!authorizeCode) return;
+
+  //   axios
+  //     .post(
+  //       `https://www.dropbox.com/oauth2/token`,
+  //       {},
+  //       {
+  //         params: {
+  //           code: authorizeCode,
+  //           redirect_uri: config.DROPBOX_REDIRECT_URI,
+  //           client_id: config.DROPBOX_CLIENT_ID,
+  //           client_secret: config.DROPBOX_CLIENT_SECRET,
+  //           grant_type: 'authorization_code',
+  //         },
+  //       }
+  //     )
+  //     .then(response => console.log(response.data))
+  //     // .then(data => {
+  //     //   return axios.post(
+  //     //     'http://localhost:8000/api/storage-provider',
+  //     //     {
+  //     //       ...data,
+  //     //       type: 'Dropbox',
+  //     //       connection_label: 'Dropbox-1',
+  //     //     },
+  //     //     {
+  //     //       headers: {
+  //     //         Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0aGluaHRiQGdtYWlsLmNvbSIsImV4cCI6MTcyMDU4MzE2OSwidHlwZSI6ImFjY2VzcyJ9.r6kbdaUm-m9baDToDHZWRCAzsIr2njS1NQJX1B0zHpw`,
+  //     //       },
+  //     //     }
+  //     //   );
+  //     // })
+  //     .catch(error => console.log(error));
+  // }, [authorizeCode]);
 
   return (
     <VStack mt="200px" gap="30px">
--- src/pages/GoogleRedirect.jsx
+++ src/pages/GoogleRedirect.jsx
@@ -1,27 +1,27 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { Heading, VStack } from '@chakra-ui/react';
-import axios from 'axios';
-import { config } from '../config';
 
 function GoogleRedirect() {
   const [querys] = useSearchParams();
   const authorizeCode = querys.get('code');
 
-  useEffect(() => {
-    if (!authorizeCode) return;
+  console.log(authorizeCode);
 
-    axios
-      .post(config.GOOGLE_TOKEN_URI, {
-        code: authorizeCode,
-        client_id: config.GOOGLE_CLIENT_ID,
-        client_secret: config.GOOGLE_CLIENT_SECRET,
-        redirect_uri: config.GOOGLE_REDIRECT_URI,
-        grant_type: 'authorization_code',
-      })
-      .then(response => console.log(response.data))
-      .catch(error => console.log(error));
-  }, [authorizeCode]);
+  // useEffect(() => {
+  //   if (!authorizeCode) return;
+
+  //   axios
+  //     .post(config.GOOGLE_TOKEN_URI, {
+  //       code: authorizeCode,
+  //       client_id: config.GOOGLE_CLIENT_ID,
+  //       client_secret: config.GOOGLE_CLIENT_SECRET,
+  //       redirect_uri: config.GOOGLE_REDIRECT_URI,
+  //       grant_type: 'authorization_code',
+  //     })
+  //     .then(response => console.log(response.data))
+  //     .catch(error => console.log(error));
+  // }, [authorizeCode]);
 
   return (
     <VStack mt="200px" gap="30px">
